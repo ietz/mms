451260aca8d1eba75db995439003efda9d8fea45
Tim Pietz <tim@pietz.me>
2018-11-03 12:57:15 +0100

    Mehrere approximationen auf dem gleichen Graphen zeichnen

--- B02 Fourierreihen und Fouriersynthese/fourier.py
+++ B02 Fourierreihen und Fouriersynthese/fourier.py
@@ -1,16 +1,34 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-p = 1000
-x = np.linspace(-1, 1, p)
-f = np.sign(x)
 
-plt.plot(x, f, color='blue', linewidth=2)
+def main():
+    p = 50001
+    x = np.linspace(-1, 1, p)
+    f = np.sign(x)
 
-result = np.zeros_like(x)
-n = 100
-for k in range(1, n, 2):
-    result = result + 4 / np.pi * np.sin(k * np.pi * x) / k
+    plt.figure(figsize=(5, 4))
+    plt.plot(x, f, linewidth=1, zorder=100, label="target")
 
-plt.plot(x, result, color='red')
-plt.show()
+    for n in [1, 5, 10, 100]:
+        g = approximate(x, n)
+        plt.plot(x, g, linewidth=1, label='n={}'.format(n))
+        print('n={:02d} has {} intersections'.format(n, len(get_intersections(x, f, g))))
+
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+def approximate(x, n):
+    result = np.zeros_like(x)
+    for k in range(1, n, 2):
+        result = result + 4 / np.pi * np.sin(k * np.pi * x) / k
+    return result
+
+
+def get_intersections(x, f, g):
+    return list(map(lambda i: x[i], np.where(np.diff(np.signbit(f - g)))[0]))
+
+
+if __name__ == '__main__':
+    main()
