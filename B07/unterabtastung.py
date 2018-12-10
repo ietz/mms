1c67b994c3b1292b33f494a1a74ce6d2b3e49a42
Tim Pietz <tim@pietz.me>
2018-12-10 21:39:27 +0100

    Fix & Plot B07A2b

--- B07/unterabtastung.py
+++ B07/unterabtastung.py
@@ -1,62 +1,76 @@
 import numpy as np
-from numpy.fft import fft
+from numpy.fft import fft, ifft
 import matplotlib.pyplot as plt
 
 
-def main():
-    N = 2000
-    n_T = 120
-    # F = 1
-    F_s = N/n_T
+N = 2000
+n_T = 120
+F_s = N/n_T
 
+
+def main():
     x = np.linspace(0, 2 * np.pi * n_T, N)
     s = np.sin(x)
 
-    for D in [1, 3, 5, 7, 8, 9, 10, 12]:
-        # Fs/D >= 2*Fmax
-        # Fmax <= Fs/(2*D)
-        f_cutoff = F_s/(2*D)
+    for D in [3, 5, 7, 8, 9, 10, 12]:
+        fig, axs = plt.subplots(2, 1)
+        fig.set_size_inches(10, 4)
 
         x_down = downsample(x, D)
+
+        # Ohne Tiefpassfilter
+        s_down = downsample(s, D)
+        s_reconstructed = interpolate(s_down, D)
+
+        axs[0].axhline(y=0, color='0.2')
+        axs[0].plot(x, s, '--', color="C1", label='original')
+        axs[0].stem(x_down, s_down, color="C2", label='downsampled')
+        axs[0].plot(x, s_reconstructed, color="C3", label='reconstructed')
+        axs[0].legend(loc='upper right')
+        axs[0].set_xlim(0, 100)
+        axs[0].set_ylim(-1.2, 1.2)
+        axs[0].set_title('without lowpass')
+
+
+        # Mit Tiefpassfilter
         s_filtered = lowpass(s, D)
-        s_down = downsample(s_filtered, D)
+        s_filtered_down = downsample(s_filtered, D)
+        s_filtered_reconstructed = interpolate(s_filtered_down, D)
 
-        s_reconstructed = interpolate(s_down, D)[:2000]
+        axs[1].axhline(y=0, color='0.2')
+        axs[1].plot(x, s, '--', color="C1", label='original')
+        axs[1].plot(x, s_filtered, color="C4", label='lowpassed')
+        axs[1].stem(x_down, s_filtered_down, color="C2", label='downsampled')
+        axs[1].plot(x, s_filtered_reconstructed, color="C3", label='reconstructed')
+        axs[1].legend(loc='upper right')
+        axs[1].set_xlim(0, 100)
+        axs[1].set_ylim(-1.2, 1.2)
+        axs[1].set_title('with lowpass')
 
-        plt.gcf().set_size_inches(10, 3)
-        plt.gca().set_xlim(0, 100)
-        plt.plot(x, s, label='original')
-        plt.plot(x, s_reconstructed, label='reconstructed')
-        plt.plot(x_down, s_down, label='downsampled')
-        plt.legend(loc='upper right')
         plt.show()
 
 
 def lowpass(s, D):
-    conv_filter = low(D)
-    return np.convolve(s, conv_filter, mode='same')
+    return ifft(fft(s) * lowpass_freq(D))
 
 
 def interpolate(s_down, D):
-    s = upsample(s_down, D)
-    conv_filter = low(D)
-    return np.convolve(s, conv_filter, mode='same')
+    return ifft(freq_extend(fft(s_down*D)))
 
 
 def downsample(s, D):
     return s[::D]
 
 
-def upsample(s, D):
-    s_up = np.zeros(len(s)*D)
-    for i in range(len(s)):
-        s_up[i*D] = s[i]
-    return s_up
+def lowpass_freq(D):
+    n_down = int(np.floor(N/D))
+    return freq_extend(np.ones(n_down))
 
 
-def low(D):
-    ns = np.arange(-1000, 1000)
-    return np.where(ns != 0, np.sinc(ns/D), 1)
+def freq_extend(f):
+    n_front = int(np.ceil(len(f)/2))
+    fill_len = np.zeros(N - len(f))
+    return np.concatenate((f[:n_front+1], fill_len, f[n_front+1:]), axis=None)
 
 
 if __name__ == '__main__':
