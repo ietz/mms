f7655555f03c54bb94944b887e7f3024b83c353a
Tim Pietz <tim@pietz.me>
2018-12-19 08:57:11 +0100

    Finalize gabor transform implementation

--- B08/gabor.py
+++ B08/gabor.py
@@ -4,45 +4,55 @@ import scipy.ndimage as spimage
 
 
 def main():
-    print("")
+    img = np.load('lena.npy')
+
+    lmbdas = [1.0, 2.0, 4.0, 6.0]
+    a_s = [0.5, 1.0, 2.0, 4.0]
+    betas = [0.125, 0.25, 0.5, 1]
+
+    for lmbda in lmbdas:
+        fig, axs = plt.subplots(len(a_s), len(betas), figsize=(10, 10))
+        fig.suptitle("lambda={}".format(lmbda))
+
+        for ia, a in enumerate(a_s):
+            for ibeta, beta in enumerate(betas):
+                x = np.arange(-32, 32)
+                y = np.arange(-32, 32)
+                xv, yv = np.meshgrid(x, y)
+
+                theta = 45*(np.pi/180)
+
+                b = 2**beta
+                w0 = 2*np.pi / lmbda
+                sgm = (a*(b+1))/(w0*(b-1))
+                wx, wy = np.cos(theta)/lmbda, -np.sin(theta)/lmbda
+
+                g = gsin(xv, yv, wx, wy, sgm)
+
+                frq = spimage.convolve(img, g)
+                frq += 128
+
+                ax = axs[ibeta][ia]
+                ax.imshow(frq, cmap='gray')
+                ax.set_title("a={}, beta={}".format(a, beta))
+
+        plt.savefig('lambda_{}.png'.format(lmbda), dpi=300)
 
 
-
-def gcos(x, y, wx, wy, b):
-    return np.sin(wx*x + wy*y) * gauss(x, sigma(wx, b)) * gauss(y, sigma(wy, b))
-
-
-def sigma(w0, b, a=1.0):
-    return (a*(b+1))/(w0*(b-1))
+def gsin(x, y, wx, wy, sgm):
+    return np.sin(wx*x + wy*y) * gauss(x, sgm) * gauss(y, sgm)
 
 
-def kernel(x, lmbda, beta, a=1.0):
-    w0 = 2*np.pi / lmbda
-    b = np.power(2, beta)
-    sigma = (a*lmbda*(b+1)) / (2*np.pi*(b-1))
-    return gauss(x, sigma)
+def gcos(x, y, wx, wy, w0, sgm):
+    gxy = gauss(x, sgm) * gauss(y, sgm)
+    nadjusted = np.cos(wx*x + wy*y) * gxy
+    a0 = np.sum(nadjusted) / np.sum(gxy)
+
+    return nadjusted - a0 * gxy
 
 
 def gauss(x, sigma):
     return 1 / np.sqrt(2 * np.pi * sigma**2) * np.exp(-x**2/(2 * sigma**2))
-
-
-img = np.load('lena.npy')
-
-
-x = np.arange(-15, 15)  # img.shape[0])
-y = np.arange(-15, 15)  # img.shape[1])
-xv, yv = np.meshgrid(x, y)
-
-g = gcos(xv, yv, 0.5, 0.5, 3)
-plt.imshow(g)
-plt.show()
-
-frq = spimage.convolve(img, g)
-frq += 128
-
-plt.imshow(frq, cmap='gray')
-plt.show()
 
 
 if __name__ == '__main__':
